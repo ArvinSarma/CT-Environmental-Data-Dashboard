48eb40a4d7fb7f2027e9c30323be12e19ab0ff1c
ArvinSarma <arvinsarma732@gmail.com>
2026-08-27 13:28:48 -0400

    added a single varaible

--- src/ct_hazardous_waste/ct_hazardous_waste_ingestion.py
+++ src/ct_hazardous_waste/ct_hazardous_waste_ingestion.py
@@ -9,7 +9,7 @@ from get_ctgov_data import get_data
 
 PROJECT_ROOT = os.path.dirname(SRC_DIR)
 CSV_PATH = os.path.join(PROJECT_ROOT,"data","Hazardous_waste.csv")
+RECORD_LIMIT = 2000000
 
 
-
-get_data("x2z6-swxe", 2000000, CSV_PATH)
+get_data("x2z6-swxe", RECORD_LIMIT, CSV_PATH)
