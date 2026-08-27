d1dd58e7e128771a430c804edb0c38a04b96c812
ArvinSarma <arvinsarma732.com>
2026-08-27 12:59:05 -0400

    made a general file for getting ctgov data

--- src/ct_hazardous_waste/ct_hazardous_waste_ingestion.py
+++ src/ct_hazardous_waste/ct_hazardous_waste_ingestion.py
@@ -1,14 +1,15 @@
 import os
 import sys
-from dotenv import load_dotenv
+
 
 # 1. PATH SETUP
 SRC_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(SRC_DIR)
+from get_ctgov_data import get_data
 
 PROJECT_ROOT = os.path.dirname(SRC_DIR)
+CSV_PATH = os.path.join(PROJECT_ROOT,"data","Hazardous_waste.csv")
 
-from get_ctgov_data import get_data
-# Load the .env file
-load_dotenv()
-get_data("x2z6-swxe", 2000000, "Hazardous_waste")
+
+
+get_data("x2z6-swxe", 2000000, CSV_PATH)
